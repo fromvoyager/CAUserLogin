a77b7e75f12ec34b8daf9c6f07cb2a25f1e52cc1
Melissa Xiao <melissaxiaoo@gmail.com>
2024-10-24 04:57:52 -0400

    task 2.1

--- src/main/java/data_access/DBUserDataAccessObject.java
+++ src/main/java/data_access/DBUserDataAccessObject.java
@@ -65,6 +65,13 @@ public class DBUserDataAccessObject implements SignupUserDataAccessInterface,
         }
     }
 
+    /**
+     * @param name a name.
+     */
+    @Override
+    public void setCurrentUser(String name) {
+    }
+
     @Override
     public boolean existsByName(String username) {
         final OkHttpClient client = new OkHttpClient().newBuilder()
